b715ae38819b0fbbd2a1d72c34acf9809174c700
heeeeing <heeeeing@naver.com>
2021-06-19 00:57:33 +0900

    main 자바 이름 변경

--- kakao_ex2/src/Main.java
+++ kakao_ex2/src/Main.java
@@ -6,7 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class KakaoDkleeApplication {
+public class Main {
 
 	public static void main(String[] args) {
 		//3[a]z
